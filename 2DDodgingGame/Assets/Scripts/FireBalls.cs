5d82162842f89fc3c19dd0d3a0301ee27aac53e1
Seo-Woongbi <suwoongby@naver.com>
2024-10-16 10:11:50 +0900

    1st MergeTest

---
+++ 2DDodgingGame/Assets/Scripts/FireBalls.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBalls : MonoBehaviour
+{
+    private bool isDestroy = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Floor"))
+        {
+            Destroy(this.gameObject, 1f);
+            isDestroy = true;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
